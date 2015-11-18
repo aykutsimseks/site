4fb306dc58925906e914a43b4fbf56c3df227293
Aykut Simsek <aykutsimseks@gmail.com>
2015-11-18 18:23:33 -0500

    Updated mover scrape script

--- aykutsimsek/apps/simseks/static/projects/moviera/imdb_latest_movie_scraper.py
+++ aykutsimsek/apps/simseks/static/projects/moviera/imdb_latest_movie_scraper.py
@@ -38,14 +38,14 @@ def mk_int(s):
     
 sleep_time=10;
     
-def get_url_content(url,page_id, loc='html', ext='html', s=sleep_time):
-    outdir	= pwd + "/html"
-    filepath 	= pwd + "/" + loc  +"/"+ page_id + '.' + ext
+def get_url_content(url,page_id, loc='html', ext='html', s=sleep_time, force=False):
+    outdir	= pwd + "/" + loc
+    filepath 	= outdir  +"/"+ page_id + '.' + ext
     
     if not os.path.exists(outdir):
         os.makedirs(outdir);
         
-    if not os.path.exists(filepath):
+    if force or not os.path.exists(filepath):
         print("Downloading page.....: " + url);
 	try:
 	    contents = urllib2.urlopen(url).read()
@@ -87,16 +87,16 @@ def html_stripper(soup,strip_tags):
             e.extract()
     return soup
 
-
-def main():
-    current = start;
+def parse_imdb_html(content, current, force=False):
     movie_list = []
-    while (current[0] + current[1].zfill(2)) <= (end[0] + end[1].zfill(2)):
-	handle = current[0] + '-' + current[1].zfill(2);
-	print handle
-	url = url_base + '/' + handle
-        content = get_url_content(url,handle)
-        soup = BeautifulSoup(content, "html.parser")
+    outdir	= pwd + "/html/parsed"
+    filepath 	= outdir + "/" + current[0] + '-' + current[1].zfill(2) + '.csv'
+    
+    if not os.path.exists(outdir):
+        os.makedirs(outdir);
+    
+    if force or not os.path.exists(filepath):
+	soup = BeautifulSoup(content, "html.parser")
 	movies = html_scraper(soup,[['tag_class','td','overview-top']])
 	images = html_scraper(soup,[['other','td','id', 'img_primary']])
 	for idx, movie in enumerate(movies):
@@ -108,50 +108,50 @@ def main():
 	    except:
 		title     = ''
 		url    	  = ''
-
+    
 	    try:
-	   	description = html_scraper(movie,[['other','div', 'itemprop','description']])[0].text.encode('utf-8').strip()
+		description = html_scraper(movie,[['other','div', 'itemprop','description']])[0].text.encode('utf-8').strip()
 	    except:
-	    	description  = ''
-		
+		description  = ''
+		    
 	    try:
 		director = html_scraper(movie,[['other','span', 'itemprop','director']])[0].text.encode('utf-8').strip()
 	    except:
 		director  = ''
-	    
+		
 	    try:
 		stars = [ g.text.encode('utf-8').strip() for g in html_scraper(movie,[['other','span', 'itemprop','actors']])]
 	    except:
 		stars = ''
-
+    
 	    try:
 		run_time   = html_scraper(movie,[['other','time', 'itemprop','duration']])[0].text.encode('utf-8').strip().split(' min')[0]
 	    except:
 		run_time  = ''
-	    
+		
 	    try:
 		genres = [ g.text.encode('utf-8').strip() for g in html_scraper(movie,[['other','span', 'itemprop','genre']])]
 	    except:
 		genres  = ''
-	    
+		
 	    try:
 		metascore = html_scraper(movie,[['tag_class','div','metascore'],['tag','strong']])[0].text.encode('utf-8').strip()
 	    except:
 		metascore  = ''
-		
-		
+		    
+		    
 	    try:
 		image_url = html_scraper(images[idx],[['other','img', 'itemprop','image']])[0].get('src').split('?')[0]
 		get_url_content(image_url,unique_id, loc='img/thumbnails', ext='png', s=1)
 	    except:
 		image_url = ''
-		
+		    
 	    try:
 		netflix_id = netflix.isNetflix(title).get('id')
 	    except:
-	    	netflix_id = ''
-		
-	    json = {
+		netflix_id = ''
+		    
+	    movie_json = {
 		'title'		: title,
 		#'url'		: url,
 		'unique_id'	: unique_id,
@@ -166,8 +166,31 @@ def main():
 		#'image_url' 	: image_url,
 		'netflix_id' 	: netflix_id
 	    }
-	    movie_list.append(json)
-	if current[1] == '12':
+	    movie_list.append(movie_json)
+	    
+	with open(filepath, 'wb') as json_file:
+	    json_file.write(json.dumps(movie_list, indent=4))
+    else:
+	with open(filepath, "rb") as json_file:
+            movie_list += json.load(json_file)
+    
+    return movie_list
+
+def main():
+    current = start;
+    end_y = int(end[0])
+    end_m = int(end[1])
+    movie_list = []
+    while (current[0] + current[1].zfill(2)) <= (end[0] + end[1].zfill(2)):
+	handle = current[0] + '-' + current[1].zfill(2);
+	cur_y = int(current[0])
+	cur_m = int(current[1])
+	print handle
+	url = url_base + '/' + handle
+	force = (cur_y*12 + cur_m) >= (end_y*12 + end_m - 1)
+        content = get_url_content(url,handle, force= force)
+	movie_list += parse_imdb_html(content, current, force= force)
+        if current[1] == '12':
 	    current[0] = str(int(current[0])+1);
 	current[1] = str((int(current[1])%12)+1);
 	

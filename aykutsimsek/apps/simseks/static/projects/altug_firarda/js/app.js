b6bbc6d962c1264df4d3815a4b62c101dcf8c400
Aykut Simsek <aykutsimseks@gmail.com>
2016-04-11 12:22:22 -0400

    Added previously seend countries.

--- aykutsimsek/apps/simseks/static/projects/altug_firarda/js/app.js
+++ aykutsimsek/apps/simseks/static/projects/altug_firarda/js/app.js
@@ -4,7 +4,7 @@
 var months = {
 	//'eng' : ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
 	//'tr'  : ["Oca", "Şub", "Mar", "Nis", "May", "Haz", "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara"],
-	
+
 	'eng' : ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
 	'tr'  : ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
 }
@@ -21,7 +21,7 @@ function formatDateInterval(s,e) {
 	var string = ""
 	if(s.getMonth() == e.getMonth()) {
 		if(s.getUTCDate() !=  e.getUTCDate()) {
-			string = s.getUTCDate() + " - " 
+			string = s.getUTCDate() + " - "
 		}
 		string += e.getUTCDate() + " " + months[tr?'tr':'eng'][e.getMonth()]
 	}
@@ -53,7 +53,7 @@ var urlParam = function(name, w) {
 }
 
 
-// Day line cross index for crossing dayline 
+// Day line cross index for crossing dayline
 var dlci = 87;
 var tr = true;
 
@@ -62,42 +62,42 @@ var create_vis = function() {
 	if (urlParam('l') == 'en') {
 		tr = false;
 	}
-	
-	
+
+
 	var marker = L.icon({
 		iconUrl: '/static/projects/altug_firarda/img/location.png',
 		iconSize: [24, 34],
 		iconAnchor: [12, 32]
 	});
 	var map;
-	
+
 	var points;
 	var line_data = []
 	var line_lookup = {};
 	var polyline;
 	var transMarkers = new L.FeatureGroup();
-		
+
 	var startDate = new Date('02/02/2015')
 	var endDate = new Date('08/21/2015');
 	var panel_width;
 	var insta_height;
-	
-	
+
+
 	var view_setup = function() {
-		panel_width  = Math.min( 
+		panel_width  = Math.min(
 			350, // Max limit
 			$('#container').width(),
 			(($('#content').height()) * 0.8)
 		)
-			
+
 		insta_height= panel_width * 1.25;
 		$('#instagram-caption').width(($('#container').width() - 20) + 'px')
 		$('#instagram-caption').height(($('#content').height() - insta_height) + "px")
-		
+
 		$('#cover').css('background-position-x', $('#container').width()/2)
 	}
 	view_setup();
-	
+
 	// Mini Map
 	function drawMiniMap() {
 		var vis_color_sets = [colorbrewer['Blues'], colorbrewer['Greens']]
@@ -107,45 +107,45 @@ var create_vis = function() {
 		world_polygons 		= null
 		countries	   		= null
 		visited = ["Turkey","United Arab Emirates","India","Sri Lanka","Maldives","Singapore","Indonesia","Malaysia","Australia","Thailand","Laos","Vietnam","Cambodia","Japan","China","Macau","Hong Kong","United States of America","Mexico","Cuba","Germany"]
-
+		old_visited=["Canada","Austria","Belgium","Bulgaria","Czech Republic","Finland","France","Germany","Greece","Italy","Liechtenstein","Monaco","Netherlands","Russia","Spain","Switzerland","United Kingdom","Vatican City"]
 
 		d3.json("/static/projects/altug_firarda/data/countries.topojson", function(error, world) {
 			world_polygons = world
-	
+
 			// Extend polygons data with csv data
 			countries = topojson.feature(world_polygons, world_polygons.objects.land).features;
 			// Parse yearly values, calculate mean and last value
 
 			// Draw maps
-			drawMap("#map_canvas1", { 'index': 1, 'color' : 'rgba(51, 122, 183,1)' })
+			drawMap("#map_canvas1", { 'index': 1, 'color' : 'rgba(51, 122, 183,1)', 'old_color' : 'rgba(	177, 207, 233,1)'})
 			$('#map-panel').hide();
-		})		
+		})
 	}
 	drawMiniMap()
 	////
-		
+
 	var add_line_icons = function(start,end, icons) {
 		var line = [];
 		var icon;
 		var prev_loc = [];
 		var curr_loc = [];
 		icons = icons||true;
-		
+
 		if(start < -1) {
 			start = 0;
-			end   = points['length']; 
+			end   = points['length'];
 			icons = false;
 		}
-		
+
 		var transportMarker = L.AwesomeMarkers.icon({
     		icon: 'plane',
-    		prefix: 'fa', 
+    		prefix: 'fa',
     		iconColor: '#990000',
-    		iconSize: [24, 34], 
+    		iconSize: [24, 34],
     		iconAnchor: [12, 16]
   		});
-  		
-  		
+
+
   		transMarkers.clearLayers();
 		for (var m = start; m < end; ++m) {
   			if(points[m]) {
@@ -156,14 +156,14 @@ var create_vis = function() {
   					line.push(L.latLng(curr_loc,true));
   					continue
   				}
-		
-  			
+
+
   				curr_loc = [points[m]['lat'],points[m]['lon']];
 	    		line.push(L.latLng(curr_loc,true));
 	    		if(prev_loc.length > 0 && icons) {
 	    			try {
 	    				transportMarker.options.icon = points[m]['arrived_by'];
-	    				var t_mark = L.marker([(points[m]['lat'] + points[m-1]['lat'])/2,(points[m]['lon'] + points[m-1]['lon'])/2],{icon: transportMarker });    				
+	    				var t_mark = L.marker([(points[m]['lat'] + points[m-1]['lat'])/2,(points[m]['lon'] + points[m-1]['lon'])/2],{icon: transportMarker });
 			    		transMarkers.addLayer(t_mark);
 			    	}
 			    	catch(err) {
@@ -173,9 +173,9 @@ var create_vis = function() {
 				prev_loc = curr_loc;
 			}
 		};
-		
+
 		map.addLayer(transMarkers);
-		
+
 		var polyline_options = {
     		color: '#990000',
     		weight: 2,
@@ -203,14 +203,14 @@ var create_vis = function() {
 		else if(start == dlci-2 || start == dlci-1) {
 			line = [[line[2],line[3],line[4]]]
 		}
-		else { 
+		else {
 			line = [line]
 		}
 		polyline = new L.multiPolyline(line, polyline_options)
 		map.addLayer(polyline);
-		
+
 	}
-	
+
 	if(tr) {
 		$('#loader span').html("&nbsp;Yükleniyor...");
 		$('#info-text').html('* Klayvenizin sağ/sol tuşlarını veya yukarıdaki ok tuşlarını kullanarak günleri değiştirebilir, ülke isimlerine tıklayarak direkt o ülkere ulaşabilirsiniz.')
@@ -219,7 +219,7 @@ var create_vis = function() {
 		$('#loader span').html("&nbsp;Loading...");
 		$('#info-text').html('* Use the left/right arrow keys on your keyboard or on the screen to navigate the map. You can click on country names above for direct access to those countries.');
 	}
-	
+
 	cartodb.createVis('map', 'http://aykutsimseks.cartodb.com/api/v2/viz/73dba65c-eb63-11e4-a391-0e9d821ea90d/viz.json', {
 			shareable		: false,
 			zoomControl		: false,
@@ -235,7 +235,7 @@ var create_vis = function() {
 				$('#map div.aside h3').html(($('#map div.cartodb-legend ul li').length -1) + " Countries")
 			}
 			map = vis.getNativeMap()
-			
+
 			map.setActiveArea({
 				position: 'absolute',
 				top: 0,
@@ -243,31 +243,31 @@ var create_vis = function() {
 				left:	$('#container').width() + 'px',
 				right: 0,
 			});
-			
+
 			$('#map div.leaflet-control-container > div.leaflet-bottom.leaflet-right > div, .cartodb-attribution li')
 			.prepend('<a href="http://www.aykutsimsek.com" target="_blank">Aykut Simsek</a> | <a href="http://cartodb.github.io/odyssey.js/index.html" target="_blank">Odyssey.js</a> | ')
 
 			new L.Control.Zoom({ position: 'topright' }).addTo(map);
-					
+
 			var seq = O.Sequential();
 			var story = O.Story();
-			var update 	= function(point,index) {				
+			var update 	= function(point,index) {
 				return O.Action(function() {
 					var pos 			= 	[point.lat, point.lon];
 					var zoom 			= 	point.zoom;
-					
+
 					$('#map-panel').hide();
-					
+
 					map.setView(pos,zoom);
-					add_line_icons(index-2,index+1)	
+					add_line_icons(index-2,index+1)
 					$('#page-counter').html((story.state() + 1) + ' / ' + (points.length + 1))
-					
-					
+
+
 					var place 			= (tr?point.place_tr:point.place);
 					var country 		= (tr?point.country_tr:point.country);
 					var title			= $.grep([place, country], Boolean).join(', ')
 					$('#place').html(title)
-					
+
 					//var begin_arr		= point.begin_date.split('T')[0].split('-')
 					//var begin 			= new Date(begin_arr.slice(0,3));
 					//var end_arr			= point.end_date.split('T')[0].split('-')
@@ -276,13 +276,13 @@ var create_vis = function() {
 					var end 			= Date.parse( point.end_date.split('T')[0])
 					var date			=  "<span class='glyphicon glyphicon-calendar'></span> " + formatDateInterval(begin,end)
 					$('#date').html(date)
-					
-					
+
+
 					var day_count		= '<div style="width:100%;text-align:center;margin-bottom:-.3em;">' + (tr?"Gün":"Day") + '</div><div style="font-size:1.5em;line-height: 1em;text-align:center;">' + (daydiff(startDate, begin) + 1) + '</div>';//(tr ? ((daydiff(startDate, begin)+1) + ". Gün") : ("Day " + (daydiff(startDate, begin) + 1)));
 					$('#day_count').html(day_count)
 					//var description 	= point.description;
-					
-					
+
+
 					var caption 		= (tr?point.story_tr:point.story);
 					if(caption) {
 						$('#instagram-caption').html(
@@ -294,14 +294,14 @@ var create_vis = function() {
 					else {
 						$('#instagram-caption').html('');
 					}
-					
+
 					var instagram_link 	=	point.instagram_links
 					if(instagram_link) {
 						current_link = instagram_link;
 						$('#loader').css("display","inline-block");
 						$('#instagram-embed').html(instagramEmbedCode(instagram_link, panel_width))
 						//$('#instagram-modal .modal-body').html(instagramEmbedCode(instagram_link, Math.min($(window).height()-300,$('#instagram-modal').width())))
-						
+
 						var modal_width = Math.min($(window).height()-240,$('#instagram-modal').width());
 						if(panel_width < 220) {
 							window.instgrm.Embeds.process();
@@ -311,7 +311,7 @@ var create_vis = function() {
 						else {
 							$('button.full-screen').prop('disabled', false);
 						}
-						
+
 						$('#instagram-modal .modal-footer-content').html("<span style='font-weight:800;font-size:1.2em;padding-right:20px;'>"  + title + "</span></br>" + date)
 						$.ajax({
 							url: 'https://api.instagram.com/oembed?url=' + instagram_link + '&hidecaption=true&maxwidth=' + modal_width,
@@ -324,14 +324,14 @@ var create_vis = function() {
 							}
 						})
 						//$('#instagram-modal .modal-body').html('<iframe height="100%" width="100%" src=' + instagram_link + '></iframe>')
-						
+
 					}
 					else {
 						$('#instagram-embed').html("")
 						$('#loader').css("display","none")
-					}	
-					
-					
+					}
+
+
 					if(index == 0) {
 						//var day_count = (daydiff(startDate, endDate)+1) + " " +(tr?"Gün":"Days");
 						$('#day_count').html('')
@@ -339,16 +339,16 @@ var create_vis = function() {
 						$('#instagram-caption').html('');
 						$('#instagram-modal .modal-body').html(caption);
 						$('#map-panel').show();
-						
+
 						$('#map-panel').clone().prependTo('#instagram-modal .modal-body');
 					}
 					view_setup();
-					
-					
+
+
 				});
 			}
-			
-			
+
+
 			O.Keys().left().then(seq.prev, seq);
 			O.Keys().right().then(seq.next, seq);
 			$('button.next').click(function() {
@@ -372,11 +372,11 @@ var create_vis = function() {
 				window.location="/altug-firarda?l=en#"+seq.current();
 			})
 
-			
-			
+
+
 			/// SET UP WELCOME PAGE
-			
-			
+
+
 			/////////
 			var start_page = {
 				'place' 	: "<i class='fa fa-globe' style='font-size:18px;'></i>&nbsp; Altug Firarda &nbsp;<i class='fa fa-globe' style='font-size:18px;'></i>",
@@ -411,7 +411,7 @@ var create_vis = function() {
 				O.Location.changeHash('#' + 0)
 			)
 			story.addState(seq.step(0), action0);
-			
+
 			var sql = new cartodb.SQL({
 				user: 'aykutsimseks'
 			});
@@ -427,30 +427,30 @@ var create_vis = function() {
 						)
 						story.addState(seq.step((i + 1)), action);
 					}
-					
+
 					if (location.hash && location.hash.slice(1)>0) {
 						seq.current(+location.hash.slice(1));
 					} else {
 						seq.current(0);
 						story.go(0);
 					}
-					
-					
+
+
 					for (var i = 0; i < points.length; i++)  {
 						var point = data.rows[i];
 						if(!(point['country'] in line_lookup)) {
 							line_data.push({
 								'color':'rgba(255,255,255,.5)',
-								//'date':point['begin_date'].split('-')[2].split('T')[0]  + "." + point['begin_date'].split('-')[1], 
-								'date':line_data.length+'.00', 
+								//'date':point['begin_date'].split('-')[2].split('T')[0]  + "." + point['begin_date'].split('-')[1],
+								'date':line_data.length+'.00',
 								'seq_id': i,
-								'id': (line_data.length+1), 
+								'id': (line_data.length+1),
 								'image': "",
 								'text':point['country'],
 								'text-offset': 0,
 								'd':[]
 							})
-							
+
 							line_lookup[point['country']] = i
 						}
 					}
@@ -458,7 +458,7 @@ var create_vis = function() {
 						story.go(line_lookup[$(this).text().trim()]+1)
 						seq.current(line_lookup[$(this).text().trim()]+1)
 					})
-					
+
 				})
 				.error(function(errors) {
 					// errors contains a list of errors
@@ -474,13 +474,13 @@ var create_vis = function() {
 function initElements(opts) {
 	var theme_color = opts['color'] || 'rgba(222,94,96,1)'
 	var index = opts.index;
-	
+
 	d3.select('#hover_text'+ index)
 		.style('text-align', 'right')//opts.index%2?'left':'right')
 		//.style('min-height', '56px')
 		.style('font-size', '13px')
 		.style('color', theme_color)
-	
+
 }
 
 function hoverUpdateElements(feature, opts) {
@@ -497,22 +497,22 @@ function hoverUpdateElements(feature, opts) {
 function drawMap(elem_id, opts) {
 	var width 	= Math.min($(elem_id).width(),$(window).height()*2);
 	var height 	= width/2;
-	
+
 	var centered;
     var locked = false;
-    
+
     var fill_color = opts['color'] || 'rgba(222,94,96,1)';
-    
+
     var projection = d3.geo.kavrayskiy7()
         //.mercator()
         .center([60, 3])
         .scale((height * 5 / 4) / Math.PI)
         .translate([(height * 5 / 4), (height * 23 / 40)])
         .precision(.1);
-    
+
     var path = d3.geo.path()
         .projection(projection);
-        
+
     var svg = d3.select(elem_id).append("svg")
         		.attr("width", width)
         		.attr("height", height)
@@ -520,14 +520,14 @@ function drawMap(elem_id, opts) {
         		//.style('display','block')
         		//.style('margin','auto')
         		//.style("border-radius", "4px")
-    
-    var g = svg.append("g") 
-    
+
+    var g = svg.append("g")
+
 	var neighbors = topojson.neighbors(world_polygons.objects.land.geometries);
-	
+
 	var highlight_opacity 	= .7;
 	var default_opacity 	= 1;
-	
+
 	var world_map = g
 		.selectAll("path")
 		.data(countries)
@@ -536,14 +536,16 @@ function drawMap(elem_id, opts) {
 		.attr('style', function(d,i){
 			var style_json = {
 					'cursor' 			: 'pointer',
-					'fill'	 			: (visited.contains(d.properties.name))?opts['color']:'#f0f0f0',
+					'fill'	 			: (visited.contains(d.properties.name))?opts['color']:(old_visited.contains(d.properties.name)?opts['old_color']:"#f0f0f0"),
 					'stroke' 			: '#444',
 					'stroke-width'		: '.4px',
 					'stroke-linejoin'	: 'round',
 					'opacity'			: default_opacity
 				}
-			
+
 			if(visited.contains(d.properties.name)) { var index = visited.indexOf(d.properties.name);visited.splice(index, 1);}
+			if(old_visited.contains(d.properties.name)) { var index = old_visited.indexOf(d.properties.name);old_visited.splice(index, 1);}
+
 			var style = ""
 			for (var s in style_json) {
 				style += s + ":" + style_json[s] + ";"
@@ -552,8 +554,9 @@ function drawMap(elem_id, opts) {
 		})
 		.attr("d", path);
 	console.log(visited)
+	console.log(old_visited)
 	initElements(opts)
-	
+
 	world_map
 		.on("mouseover", function(d) {
 			d3.select(this).style('opacity', highlight_opacity);
@@ -568,7 +571,7 @@ function drawMap(elem_id, opts) {
 			d3.select(this).classed('centered',true)
 			clicked(d);
 		})
-		
+
 	function clicked(d) {
 		var x, y, k;
 		if (d && centered != d) {
@@ -581,17 +584,17 @@ function drawMap(elem_id, opts) {
 				x = centroid[0],
 				y = centroid[1],
 				k = .4 / Math.max(dx / height, dy / height / 2);
-				
+
 			if (k < 1) k = 1.4;
 			translate = [width / 2 - k * x, height / 2 - k * y]
-			
+
 			centered = d;
 			g
 				.transition()
 				.duration(750)
 				.attr("transform", "translate(" + width * 1 / 2 + "," + height * 1 / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
 				.each("start", function() {
-					
+
 					g.selectAll("path")
 						//.style(alternative_path_style)
 						.style('opacity', highlight_opacity)
@@ -607,16 +610,16 @@ function drawMap(elem_id, opts) {
 					d3.select(elem_id + " .centered").style('opacity', default_opacity)
 				})
 				.each("end", function() {
-					
+
 					d3.select(elem_id + " .centered").style('opacity',default_opacity)
 					locked = true;
 				})
 			selected_polygons[opts.index] = d;
-		} 
+		}
 		else {
 			k = 1;
 			centered = null;
-			
+
 			d3.select(elem_id + " .centered").classed('centered', false)
 			g
 				.transition()
@@ -635,9 +638,9 @@ function drawMap(elem_id, opts) {
 						})
 				})
 				.each("end", function() {
-					locked=false;		
+					locked=false;
 				})
 			selected_polygons[opts.index] = null;
 		}
-	}        
+	}
 }
